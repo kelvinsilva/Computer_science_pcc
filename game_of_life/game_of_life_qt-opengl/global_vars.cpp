e1c538992fa3389f91eae6d342e5e66c3ace6f26
Kelvin Silva <kelvinsilva747@gmail.com>
2013-12-06 21:37:51 +0000

    global_vars.cpp edited online with Bitbucket

--- game_of_life/game_of_life_qt-opengl/global_vars.cpp
+++ game_of_life/game_of_life_qt-opengl/global_vars.cpp
@@ -1,5 +1,6 @@
 #include "global_vars.h"
 #include "rule.h"
+//global variables for usage across panelpoengl and mainwindow
     int iteration_count = 0;
-    int delay = 100;
-    int x_ = 0, y_ = 0, x_x = WORLDSIZE-1, y_y = WORLDSIZE-1;
+    int delay = 100;    //delay for frame rate.
+    int x_ = 0, y_ = 0, x_x = WORLDSIZE-1, y_y = WORLDSIZE-1; //four coordinates needed to capture section
